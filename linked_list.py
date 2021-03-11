61cf8e91401938a0bde1a59d96e6219cd31cfd26
caspero-62 <nwaforuchechukwu2007@gmail.com>
2021-03-11 11:18:03 +0100

    second commit

--- linked_list.py
+++ linked_list.py
@@ -4,13 +4,18 @@ class Node(object):
         self.next = None
 
 class LinkedList(object):
+    #initialize linked list's head to be None
     def __init__(self, head=None):
         self.head = head
 
     def append(self, data):
+        #create a new node from the data to be appended
         new_node = Node(data)
         current = self.head
+        # satisfy the following condition if linked list has
+        # a head already, otherwise make the new node the head
         if self.head:
+            #loop through the linked list and append the new list to the end. 
             while current.next:
                 current = current.next
             current.next = new_node
@@ -23,7 +28,7 @@ class LinkedList(object):
         while current.value != data and current.next:
             previous = current
             current = current.next
-        if current.value == data:
+        if previous and current.value == data:
             previous.next = current.next
         else:
             self.head = current.next
@@ -61,7 +66,7 @@ class LinkedList(object):
             current = current.next
         return total
 
-    def getPosition(self, position):
+    def getPositionValue(self, position):
         current = self.head
         count = 1
         if position < 1:
@@ -81,9 +86,10 @@ print(my_list.displayList())
 my_list.insert(3, 3)
 print(my_list.getLength())
 print(my_list.displayList())
-print(my_list.getPosition(3))
-my_list.delete(2)
+print(my_list.getPositionValue(3))
+my_list.delete(1)
 print(my_list.getLength())
 print(my_list.displayList())
+print(my_list.getPositionValue(3))
 
         
